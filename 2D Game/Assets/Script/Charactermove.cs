44f15978ed8ad1c83c47fd8859bcc73e47ab7b51
Spencer Deschamps <spencer.d320@stu.nebo.edu>
2018-09-13 10:29:09 -0600

    2D Game
    Added jump and move to PC

--- 2D Game/Assets/Script/Charactermove.cs
+++ 2D Game/Assets/Script/Charactermove.cs
@@ -4,16 +4,39 @@ using UnityEngine;
 
 public class Charactermove : MonoBehaviour {
 
-	public int moveSpeed = 10; 
+	// Player Movement Variables
+	public int MoveSpeed;
+	public float Jumpheight; 
 
+	//Player grounded variation
+	public Transform groundcheck;
+	public float groundcheckradius;
+	public LayerMask whatisground;
+	private bool grounded;
 
+	void FixedUpdate () {
+		grounded = Physics2D.OverlapCircle(groundcheck.position, groundcheckradius, whatisground);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// this code makes the character jump
+		if(Input.GetKeyDown (KeyCode.W)&& grounded){
+			Jump();
+		}
+		// This code makes the character move from side to side using a&d
+		if(Input.GetKey (KeyCode.D)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		}
+		if(Input.GetKey (KeyCode.A)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.x);
+		}
+}
 	// Use this for initialization
 	void Start () {
 		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	}	
+		public void Jump(){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, Jumpheight);
+		}
 }
