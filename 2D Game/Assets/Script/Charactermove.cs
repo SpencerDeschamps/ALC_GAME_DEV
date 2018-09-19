098208d7aff0c2e47f909324851681cb31e42f44
Spencer Deschamps <spencer.d320@stu.nebo.edu>
2018-09-19 10:29:04 -0600

    Jump and Stick
    Added double jump and Stick to character so he won't slide when moving

--- 2D Game/Assets/Script/Charactermove.cs
+++ 2D Game/Assets/Script/Charactermove.cs
@@ -7,12 +7,16 @@ public class Charactermove : MonoBehaviour {
 	// Player Movement Variables
 	public int MoveSpeed;
 	public float Jumpheight; 
+	private bool DoubleJump;
 
 	//Player grounded variation
 	public Transform groundcheck;
 	public float groundcheckradius;
 	public LayerMask whatisground;
 	private bool grounded;
+
+	//non slide player
+	private float MoveVelocity;
 
 	void FixedUpdate () {
 		grounded = Physics2D.OverlapCircle(groundcheck.position, groundcheckradius, whatisground);
@@ -29,8 +33,31 @@ public class Charactermove : MonoBehaviour {
 			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		}
 		if(Input.GetKey (KeyCode.A)){
-			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.x);
+			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		}
+		// Double Jump Code
+		if(grounded)
+		DoubleJump = false;
+
+		if(Input.GetKeyDown (KeyCode.Space)&& !DoubleJump && !grounded){
+			Jump();
+			DoubleJump = true;
+		}
+	// Non-slide player
+	MoveVelocity = 0f;
+
+	// This code makes the character move from side to side using a&d
+	if(Input.GetKey (KeyCode.D)){
+		//GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		MoveVelocity = MoveSpeed;
+	}
+	if(Input.GetKey (KeyCode.A)){
+		//GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+		MoveVelocity = -MoveSpeed;	
+	}
+
+	GetComponent<Rigidbody2D>().velocity = new Vector2(MoveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+
 }
 	// Use this for initialization
 	void Start () {
