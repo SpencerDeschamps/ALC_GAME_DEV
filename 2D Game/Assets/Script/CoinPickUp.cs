43e430a6a67b20ff9434fd90590b6ac0152d8ccc
Spencer Deschamps <spencer.d320@stu.nebo.edu>
2018-09-25 10:29:13 -0600

    Added coin and score
    Coin works fine but still need to get score working.

---
+++ 2D Game/Assets/Script/CoinPickUp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class CoinPickUp : MonoBehaviour {
+	public int PointsToAdd;
+
+	void OnTriggerEnter2D (Collider2D other){
+
+			if (other.GetComponent<Rigidbody2D> () == null)
+				return;
+
+			ScoreManager.AddPoints (PointsToAdd);
+
+			Destroy (gameObject);
+		}
+}	
