02670305a8a3ad395ca5e433dea711066fb01fb3
Spencer Deschamps <spencer.d320@stu.nebo.edu>
2018-09-27 10:29:36 -0600

    Working on an Enemy
    Code doesnt work 100% yet and enemy isnt ready.

---
+++ 2D Game/Assets/Script/EmenyPatrol.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmenyPatrol : MonoBehaviour {
+
+	// Movement Variables
+	public float MoveSpeed;
+	public bool MoveRight;
+
+	// Wall Check
+	public Transform WallCheck;
+	public float WallCheckRadius;
+	public LayerMask WhatIsWall;
+	private bool HittingWall;
+	
+	// Edge Check
+	private bool NotAtEdge;
+
+	public Transform EdgeCheck;
+
+	
+	// Update is called once per frame
+	void Update () {
+		NotAtEdge = Physics2D.OverlapCircle(EdgeCheck.position, WallCheckRadius, WhatIsWall);
+
+		HittingWall = Physics2D.OverlapCircle(WallCheck.position, WallCheckRadius, WhatIsWall);
+
+		if (HittingWall || !NotAtEdge){
+			MoveRight = !MoveRight;
+		}	
+
+		if (MoveRight){
+			transform.localScale = new Vector3(-0.2f,0.2f,1f);
+			GetComponent<RigidBody2D>().velocity = new Vector2(MoveSpeed, GetComponent<RigidBody2D>().velocity.y);
+		}
+		else {
+			transform.localScale = new Vector3(0.2f,0.2f,1f);
+			GetComponent<RigidBody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<RigidBody2D>().velocity.y);
+		}
+	}
+}
