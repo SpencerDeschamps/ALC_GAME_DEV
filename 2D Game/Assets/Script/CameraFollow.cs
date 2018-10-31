60db529d0077598a9281cf1d5d05c44345b01d94
Spencer Deschamps <spencer.d320@stu.nebo.edu>
2018-10-31 10:22:28 -0600

    did stuff

---
+++ 2D Game/Assets/Script/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Charactermove Player;
+
+	public bool isFollowing;
+
+	// camera offset
+	public float xOffset;
+	public float yOffset;
+
+	// Use this for initialization
+	void Start () {
+		Player = FindObjectOfType<Charactermove>();
+
+		isFollowing = true;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(isFollowing){
+			transform.position = new Vector3(Player.transform.position.x + xOffset, Player.transform.position.y + yOffset, transform.position.z);
+		}
+	}
+}
