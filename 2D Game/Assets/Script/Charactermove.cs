3fc1bf717f1d53c7df674d53b9b896ddb3c127e4
Spencer Deschamps <spencer.d320@stu.nebo.edu>
2018-12-11 10:28:18 -0700

    tried to do things but broke game

--- 2D Game/Assets/Script/Charactermove.cs
+++ 2D Game/Assets/Script/Charactermove.cs
@@ -22,12 +22,16 @@ public class Charactermove : MonoBehaviour {
 		grounded = Physics2D.OverlapCircle(groundcheck.position, groundcheckradius, whatisground);
 	}
 
+	public Animator animator;
+
 	// Update is called once per frame
 	void Update () {
 		// this code makes the character jump
 		if(Input.GetKeyDown (KeyCode.W)&& grounded){
 			Jump();
 		}
+
+
 		// This code makes the character move from side to side using a&d
 		if(Input.GetKey (KeyCode.D)){
 			GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
@@ -36,8 +40,10 @@ public class Charactermove : MonoBehaviour {
 			GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		}
 		// Double Jump Code
-		if(grounded)
-		DoubleJump = false;
+		if(grounded){
+			DoubleJump = false;
+		
+		}
 
 		if(Input.GetKeyDown (KeyCode.W)&& !DoubleJump && !grounded){
 			Jump();
@@ -50,6 +56,10 @@ public class Charactermove : MonoBehaviour {
 	if(Input.GetKey (KeyCode.D)){
 		//GetComponent<Rigidbody2D>().velocity = new Vector2(MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
 		MoveVelocity = MoveSpeed;
+
+	else if (Input.GetKeyUp (KeyCode.D)){
+		animator.SetBool("IsWalking",false);
+
 	}
 	if(Input.GetKey (KeyCode.A)){
 		//GetComponent<Rigidbody2D>().velocity = new Vector2(-MoveSpeed, GetComponent<Rigidbody2D>().velocity.y);
@@ -68,9 +78,12 @@ public class Charactermove : MonoBehaviour {
 }
 	// Use this for initialization
 	void Start () {
-		
+		animator.SetBool("IsWalking",false);
+		animator.SetBool("IsJumping",false);
 	}	
 		public void Jump(){
 			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, Jumpheight);
+			animator.SetBool("IsJumping",true);
 		}
+		
 }
